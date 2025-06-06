613b95f540d5ee8ed9ce33d32d646db7921c9a4e
LuisFc11 <luisangelflorescruz7@gmail.com>
2025-06-06 09:56:47 -0500

    terminado

--- ClasificacionCV/db/db.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-def conectar_bd():
-    return mysql.connector.connect(
-        host="localhost:3300",
-        user="root",
-        password="110206",
-        database="clasificacion_cv"
-    )
